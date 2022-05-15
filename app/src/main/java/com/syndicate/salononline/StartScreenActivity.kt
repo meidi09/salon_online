88fc2270d984c451591fb3d463f0edb03e438992
meidi09 <meidikandowangko09@gmail.com>
2022-05-15 23:23:47 +0800

    Salon Online

---
+++ app/src/main/java/com/syndicate/salononline/StartScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.syndicate.salononline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+    }
+}
